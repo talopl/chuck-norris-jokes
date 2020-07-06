109661682256962cc0700e797256c791ba28339b
talopl <taltal2905@gmail.com>
2020-07-06 22:53:48 +0300

    Updates

---
+++ precache-manifest.4bbf26eb07ebba3e35ea67d560e344d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5738e97a7337d4e481d857ae04ddf447",
+    "url": "/chuck-norris-jokes/index.html"
+  },
+  {
+    "revision": "acf2c168cadaad3ff7d8",
+    "url": "/chuck-norris-jokes/static/css/main.48ec7b5f.chunk.css"
+  },
+  {
+    "revision": "d0583ffa66155b5ed623",
+    "url": "/chuck-norris-jokes/static/js/2.4b4503cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck-norris-jokes/static/js/2.4b4503cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf2c168cadaad3ff7d8",
+    "url": "/chuck-norris-jokes/static/js/main.e375a8af.chunk.js"
+  },
+  {
+    "revision": "a882e51e526507030c52",
+    "url": "/chuck-norris-jokes/static/js/runtime-main.61d3aefd.js"
+  }
+]);
