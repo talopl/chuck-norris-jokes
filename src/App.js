fd35fcaf79a93de67f80bc5d1bf23500635a04b9
talopl <taltal2905@gmail.com>
2020-07-07 22:28:48 +0300

    changed title to chuck norris jokes

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  const BASE_URL = 'https://api.chucknorris.io/jokes/random'
+
+  const initialJokeValue = "Loading . . . "
+
+  const [joke, setJoke] = useState(() => initialJokeValue)
+
+  function fetchJoke() {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => setJoke(value => data['value']))
+  }
+  
+  useEffect(() => {
+    fetchJoke()
+  }, [])
+
+  function changeJoke(){
+    setJoke(initialJokeValue)
+    fetchJoke()
+  }
+  return (
+    <>
+      <div className="joke-container">
+        <div className="title">The Funniest Joke Ever</div>
+        <div className="joke">
+          {joke}
+        </div>
+        <button className="generate-joke-btn" onClick={changeJoke}>New Joke</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
